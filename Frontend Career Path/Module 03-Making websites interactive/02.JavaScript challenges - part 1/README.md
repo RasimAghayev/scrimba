97132381fbad0f34e3f3baba2a0a060486fe6e59
Rasim Aghayev <rasimaqayev@gmail.com>
2024-06-27 23:11:49 +0400

    02. Concatenate two strings in a function - 0:58

--- Frontend Career Path/Module 03-Making websites interactive/02.JavaScript challenges - part 1/README.md
+++ Frontend Career Path/Module 03-Making websites interactive/02.JavaScript challenges - part 1/README.md
@@ -1,7 +1,7 @@
 JavaScript challenges - part 1 - 10 lessons - 26 min
 
 [x] 01. Variables practice - 1:52
-[ ] 02. Concatenate two strings in a function - 0:58
+[x] 02. Concatenate two strings in a function - 0:58
 [ ] 03. Incrementing and decrementing - 2:11
 [ ] 04. Strings and numbers - 1:34
 [ ] 05. Rendering an error message - 3:15
--- Frontend Career Path/Module 03-Making websites interactive/02.JavaScript challenges - part 1/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/02.JavaScript challenges - part 1/index.js
@@ -1,9 +1,10 @@
-// Create two variables, firstName and lastName
-let firstName = "Per";
-let lastName = "Borgen";
-// Concatenate the two variables into a third variable called fullName
+let name = "Linda";
+let greeting = "Hi there";
 
-let fullName = firstName + " " + lastName;
+// Create a function that logs out "Hi there, Linda!" when called
 
-// Log fullName to the console
-console.log(fullName);
+function greetLinda() {
+  console.log(greeting + ", " + name + "!");
+}
+
+greetLinda();
