c84d61395d9abb45d222877886f67e3ad6087aa6
Rasim Aghayev <rasimaqayev@gmail.com>
2024-06-28 00:24:09 +0400

    11. Add the message variable - 3:21

--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/README.md
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/README.md
@@ -11,7 +11,7 @@ Build a Blackjack game - 55 lessons - 2 hours 45 min
 [x] 08. Add the hasBlackJack variable - 2:03
 [x] 09. Add the isAlive variable - 1:44
 [x] 10. Let's practice boolean conditions - 3:25
-[ ] 11. Add the message variable - 3:21
+[x] 11. Add the message variable - 3:21
 [ ] 12. Link to stylesheet - 2:32
 [ ] 13. Add basic styling - 4:18
 [ ] 14. Make the start button work - 5:49
--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
@@ -1,25 +1,21 @@
-console.log(4 === 3); // false
-console.log(5 > 2); // true
-console.log(12 > 12); // false
-console.log(3 < 0); // false
-console.log(3 >= 3); // true
-console.log(11 <= 11); // true
-console.log(3 <= 2); // false
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+// 1. Declare a variable called message and assign its value to an empty string
+let message = "";
 
-// let firstCard = 10
-// let secondCard = 11
-// let sum = firstCard + secondCard
-// let hasBlackJack = false
-// let isAlive = true
+// 2. Reassign the message variable to the string we're logging out
+if (sum <= 20) {
+  message = "Do you want to draw a new card? 🙂";
+} else if (sum === 21) {
+  message = "Wohoo! You've got Blackjack! 🥳";
+  hasBlackJack = true;
+} else {
+  message = "You're out of the game! 😭";
+  isAlive = false;
+}
 
-// if (sum <= 20) {
-//     console.log("Do you want to draw a new card? 🙂")
-// } else if (sum === 21) {
-//     console.log("Wohoo! You've got Blackjack! 🥳")
-//     hasBlackJack = true
-// } else {
-//     console.log("You're out of the game! 😭")
-//     isAlive = false
-// }
-
-// console.log(isAlive)
+// 3. Log it out!
+console.log(message);
