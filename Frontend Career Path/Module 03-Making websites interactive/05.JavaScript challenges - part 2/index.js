e8dc67dfaff721c8f6b80c18f4d041d84291af78
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-06-28 17:25:56 +0400

    06. Rock papers scissors - 2:59

--- Frontend Career Path/Module 03-Making websites interactive/05.JavaScript challenges - part 2/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/05.JavaScript challenges - part 2/index.js
@@ -1,12 +1,10 @@
-let dayOfMonth = 13
-let weekday = "Tuesday"
+let hands = ["rock", "paper", "scissor"]
 
-// If it is Friday the 13th, log out this spooky face: 😱
-// Use the logical "AND operator" -> &&
+// Create a function that returns a random item from the array
 
-if (dayOfMonth === 13 && weekday === "Friday") {
-    console.log("😱")
+function getHand() {
+    let randomIndex = Math.floor( Math.random() * 3)
+    return hands[randomIndex]
 }
 
-
-
+console.log( getHand() )
