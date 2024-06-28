fc81b182bc80719335e1dcb738fd0104a17b6e3f
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-06-28 12:35:11 +0400

    25. Aside: Array.push() and .pop() - 4:20

--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/README.md
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/README.md
@@ -25,7 +25,7 @@ Build a Blackjack game - 55 lessons - 2 hours 45 min
 [x] 22. Aside: Intro to arrays - 5:24
 [x] 23. Aside: Array indexes - 4:03
 [x] 24. Arrays with multiple data types - 2:18
-[ ] 25. Aside: Array.push() and .pop() - 4:20
+[x] 25. Aside: Array.push() and .pop() - 4:20
 [ ] 26. Creating the cards array - 1:04
 [ ] 27. Push a new card to the array - 1:18
 [ ] 28. Aside: Loops - 4:01
--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
@@ -1,6 +1,21 @@
-// Array - ordered list of items - coposite / complex data type
+let cards = [7, 4]
+cards.push(6)
+//console.log(cards)
 
-// Create an array that describes yourself. Use the three primitive data types you've learned
-// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+// Push the newMessage to the messages array, and then log out the array
 
-let per = ["Per Harald Borgen", 35, true]
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+console.log(messages)
+
+// How can you remove the last item in an array? Try to google it!
+messages.pop()
+console.log(messages)
+
+
