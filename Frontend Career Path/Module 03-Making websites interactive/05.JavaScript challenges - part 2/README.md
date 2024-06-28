964a21c4421d6a34985db9e9eb17c81fb199ecd4
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-06-28 17:46:40 +0400

    08. Sorting fruits - 2:57

--- Frontend Career Path/Module 03-Making websites interactive/05.JavaScript challenges - part 2/README.md
+++ Frontend Career Path/Module 03-Making websites interactive/05.JavaScript challenges - part 2/README.md
@@ -8,5 +8,5 @@ JavaScript challenges - part 2 - 9 lessons - 25 min
 [x] 05. Logical operators - 1:56
 [x] 06. Rock papers scissors - 2:59
 [x] 07. EmojiFighter - 4:32
-[ ] 08. Sorting fruits - 2:57
+[x] 08. Sorting fruits - 2:57
 [ ] 09. Solo Project (PRO) - Password Generator - 2:43
--- Frontend Career Path/Module 03-Making websites interactive/05.JavaScript challenges - part 2/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/05.JavaScript challenges - part 2/index.js
@@ -1,13 +1,19 @@
-let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
 
-let stageEl = document.getElementById("stage")
-let fightButton = document.getElementById("fightButton")
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
 
-fightButton.addEventListener("click", function() {
-    // Challenge:
-    // When the user clicks on the "Pick Fighters" button, pick two random 
-    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
-    let randomIndexOne = Math.floor( Math.random() * fighters.length )
-    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
-    stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
-})
+function sortFruit() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊"
+        }
+    }
+}
+
+sortFruit()
