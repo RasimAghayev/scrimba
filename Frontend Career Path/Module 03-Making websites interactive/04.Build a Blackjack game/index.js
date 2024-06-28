cd3396e4e3ba5c47507728be31c8cb61aa8dd094
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-06-28 13:24:44 +0400

    31. Write your first array-based for loop - 1:29

--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
@@ -1,20 +1,8 @@
-let messages = [
-    "Hey, how's it going?",        
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately.",
-    "Same here!",
-    "Great to hear",
-    "🙌"
-]
+let cards = [7, 3, 9]
 
-// DRY - Don't repeat yourself
-// console.log(messages[0])
-// console.log(messages[1])
-// console.log(messages[2])
-// console.log(messages[3])
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
 
-for (let i = 0; i < messages.length; i += 1) {
-    console.log(messages[i])
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i])
 }
-
-
