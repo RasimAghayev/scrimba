0638c4559e0716463d9960200ed15f9788b172e6
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-06-28 13:18:01 +0400

    28. Aside: Loops - 4:01

--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/README.md
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/README.md
@@ -28,7 +28,7 @@ Build a Blackjack game - 55 lessons - 2 hours 45 min
 [x] 25. Aside: Array.push() and .pop() - 4:20
 [x] 26. Creating the cards array - 1:04
 [x] 27. Push a new card to the array - 1:18
-[ ] 28. Aside: Loops - 4:01
+[x] 28. Aside: Loops - 4:01
 [ ] 29. Write your first loop - 3:22
 [ ] 30. Aside: For loops and arrays - 3:43
 [ ] 31. Write your first array-based for loop - 1:29
--- Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/04.Build a Blackjack game/index.js
@@ -1,39 +1,17 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+// Count to ten!
 
-function startGame() {
-    renderGame()
-}
+// We need to specify...
 
-function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+for ( let count = 10;  count < 21;  count += 1 )  {
+    
+    console.log(count)
+
 }
 
 
-function newCard() {
-    let card = 6
-    sum += card
-    // Push the card to the cards array
-    cards.push(card)
-    console.log(cards)
-    renderGame()
-}
+
