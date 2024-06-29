74e7a19462c545b5959046e42ed512e6d2fdbb25
Rasim Aghayev <rasimaqayev@gmail.com>
2024-06-30 01:11:48 +0400

    49. Arrays as parameters - 1:54

--- Frontend Career Path/Module 03-Making websites interactive/06.Build a Chrome Extension/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/06.Build a Chrome Extension/index.js
@@ -73,3 +73,14 @@ function add(num1, num2) {
 }
 
 add(3, 4);
+// Create a function, getFirst(arr), that returns the first item in the array
+
+function getFirst(arr) {
+  return arr[0];
+}
+
+let firstCard = getFirst([10, 2, 5]);
+
+console.log(firstCard);
+
+// Call it with an array as an argument to verify that it works
