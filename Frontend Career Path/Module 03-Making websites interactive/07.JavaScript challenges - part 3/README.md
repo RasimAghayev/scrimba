2021208647e03a62f5e4abf90a9a7ab82bd8b069
Rasim Aghayev <rasimaqayev@gmail.com>
2024-06-30 01:54:16 +0400

    06. Render images - 6:05

--- Frontend Career Path/Module 03-Making websites interactive/07.JavaScript challenges - part 3/README.md
+++ Frontend Career Path/Module 03-Making websites interactive/07.JavaScript challenges - part 3/README.md
@@ -6,7 +6,7 @@ JavaScript challenges - part 3 - 9 lessons - 26 min
 [x] 03. save to localStorage - 1:53
 [x] 04. addEventListener and object in array - 2:39
 [x] 05. Generate sentence - 4:53
-[ ] 06. Render images - 6:05
+[x] 06. Render images - 6:05
 [ ] 07. Rounding numbers - 1:45
 [ ] 08. Convert string to number challenge - 2:24
 [ ] 09. Solo Project (PRO) - Unit converter - 1:54
--- Frontend Career Path/Module 03-Making websites interactive/07.JavaScript challenges - part 3/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/07.JavaScript challenges - part 3/index.js
@@ -1,25 +1,17 @@
-// The generateSentence(desc, arr) takes two parameterer: a description and an array.
-// It should return a string based upon the description and array.
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
 
-// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
-// it should return the string: "The 3 largest countries are China, India, USA"
+const imgs = ["images/hip1.jpg", "images/hip2.jpg", "images/hip3.jpg"];
 
-// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
-// "The 2 best fruits are Apples, Bananas"
+const container = document.getElementById("container");
 
-// Use both a for loop and a template string to solve the challenge
-function generateSentence(desc, arr) {
-  let baseString = `The ${arr.length} ${desc} are `;
-  const lastIndex = arr.length - 1;
-  for (let i = 0; i < arr.length; i++) {
-    if (i === lastIndex) {
-      baseString += arr[i];
-    } else {
-      baseString += arr[i] + ", ";
-    }
+function renderImages() {
+  let imgsDOM = "";
+  for (let i = 0; i < imgs.length; i++) {
+    imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`;
   }
-  return baseString;
+  container.innerHTML = imgsDOM;
 }
 
-const sentence = generateSentence("highest mountains", ["Mount Everest", "K2"]);
-console.log(sentence);
+renderImages();
