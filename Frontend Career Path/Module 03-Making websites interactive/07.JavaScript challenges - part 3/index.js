aa0bdbcb94d73152d14b5fc9b8be76297a27fdc3
Rasim Aghayev <rasimaqayev@gmail.com>
2024-06-30 01:43:57 +0400

    03. save to localStorage - 1:53

--- Frontend Career Path/Module 03-Making websites interactive/07.JavaScript challenges - part 3/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/07.JavaScript challenges - part 3/index.js
@@ -1,17 +1,8 @@
-let myCourses = [
-  "Learn CSS Animations",
-  "UI Design Fundamentals",
-  "Intro to Clean Code",
-];
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
 
-// Create a function that takes a single parameter, an array,
-// and logs all the items of the array to the console.
-// Call the function while passing in myCourses as an argument
+localStorage.setItem("myCredits", "100");
 
-function logItems(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
-
-logItems(myCourses);
+let myCredits = localStorage.getItem("myCredits");
+console.log(myCredits);
