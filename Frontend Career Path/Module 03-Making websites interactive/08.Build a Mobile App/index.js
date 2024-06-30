2a50441b83fad92546deeeca71d638af60bbdb10
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-01 01:54:35 +0400

    Render all items using onValue

--- Frontend Career Path/Module 03-Making websites interactive/08.Build a Mobile App/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/08.Build a Mobile App/index.js
@@ -1,9 +1,47 @@
-const loginCredentials = {
-  rafidhoda: "BestPassword123",
-  shahrukhkhan: "InBigCitiesSmallThingsHappen",
-  jackblack: "ThisIsNotTheGreatestPasswordInTheWorld",
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL: process.env.DATABASE_URL,
 };
 
-console.log(Object.keys(loginCredentials));
-console.log(Object.values(loginCredentials));
-console.log(Object.entries(loginCredentials));
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "leads");
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+onValue(referenceInDB, function (snapshot) {
+  const snapshotValues = snapshot.val();
+  const leads = Object.values(snapshotValues);
+  render(leads);
+});
+
+deleteBtn.addEventListener("dblclick", function () {});
+
+inputBtn.addEventListener("click", function () {
+  push(referenceInDB, inputEl.value);
+  inputEl.value = "";
+});
