f6b775634861f4eaa131d2b35270ac5971c5eb8c
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-01 01:52:37 +0400

    Aside: Turn an Object into an Array

--- Frontend Career Path/Module 03-Making websites interactive/08.Build a Mobile App/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/08.Build a Mobile App/index.js
@@ -1,46 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-  onValue,
-} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-
-const firebaseConfig = {
-  databaseURL: process.env.DATABASE_URL,
+const loginCredentials = {
+  rafidhoda: "BestPassword123",
+  shahrukhkhan: "InBigCitiesSmallThingsHappen",
+  jackblack: "ThisIsNotTheGreatestPasswordInTheWorld",
 };
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const referenceInDB = ref(database, "leads");
-
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
-
-function render(leads) {
-  let listItems = "";
-  for (let i = 0; i < leads.length; i++) {
-    listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `;
-  }
-  ulEl.innerHTML = listItems;
-}
-
-// Challenge: Log out a snapshot of your database when a new value is added to it
-onValue(referenceInDB, function (snapshot) {
-  console.log(snapshot.val());
-});
-
-deleteBtn.addEventListener("dblclick", function () {});
-
-inputBtn.addEventListener("click", function () {
-  push(referenceInDB, inputEl.value);
-  inputEl.value = "";
-});
+console.log(Object.keys(loginCredentials));
+console.log(Object.values(loginCredentials));
+console.log(Object.entries(loginCredentials));
