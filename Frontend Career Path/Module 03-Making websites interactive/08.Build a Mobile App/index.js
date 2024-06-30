8ffebb7bb9abca1686694d41ff28f1c4395d9281
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-01 01:41:44 +0400

    Aside: Push data to Firebase Realtime Database

--- Frontend Career Path/Module 03-Making websites interactive/08.Build a Mobile App/index.js
+++ Frontend Career Path/Module 03-Making websites interactive/08.Build a Mobile App/index.js
@@ -1,74 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   databaseURL:
-    "https://leads-tracker-app-75a41-default-rtdb.europe-west1.firebasedatabase.app/",
-  // apiKey: "AIzaSyBW8hK69UgM-F-LUd0CRd74UVLPzM2k0cs",
-  // authDomain: "leads-tracker-app-75a41.firebaseapp.com",
-  // projectId: "leads-tracker-app-75a41",
-  // storageBucket: "leads-tracker-app-75a41.appspot.com",
-  // messagingSenderId: "432218779852",
-  // appId: "1:432218779852:web:664fea2d266f0e0c729269",
-  // measurementId: "G-QDV42ECF7M",
+    "https://birthday-app-75b25-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
+const referenceInDB = ref(database, "birthdays");
 
-console.log(database);
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-const tabBtn = document.getElementById("tab-btn");
+const birthdayInputField = document.getElementById("birthday-input");
+const submitButton = document.getElementById("submit-button");
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
-}
-
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-});
-
-function render(leads) {
-  let listItems = "";
-  for (let i = 0; i < leads.length; i++) {
-    listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `;
-  }
-  ulEl.innerHTML = listItems;
-}
-
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
-});
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
+submitButton.addEventListener("click", function () {
+  push(referenceInDB, birthdayInputField.value);
+  birthdayInputField.value = "";
 });
