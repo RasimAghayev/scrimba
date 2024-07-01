ec0fa0550a4cc313342fab326b3a9b64cbfa6fa6
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-07-01 15:14:52 +0400

    Aside: Accessible JavaScript

--- Frontend Career Path/Module 03.1-Accessible development/index.js
+++ Frontend Career Path/Module 03.1-Accessible development/index.js
@@ -1,14 +1,32 @@
-function sendMessage() {
-    // Get the button element
-    const button = document.getElementById('submitButton');
+function checkInputs() {
+    const name = document.getElementById('name').value;
+    const address = document.getElementById('address').value;
+    const city = document.getElementById('city').value;
+    const postcode = document.getElementById('postcode').value;
+    const email = document.getElementById('email').value;
+    const yesRadio = document.getElementById('yes').checked;
+    const noRadio = document.getElementById('no').checked;
+
+    // Checks if all fields are filled (truthy) and at least one radio button is selected
+    const allFilled = name && address && city && postcode && email && (yesRadio || noRadio);
     
-    // Replace the button with a paragraph
-    button.outerHTML = '<p id="submitMessage" class="submit-message" aria-live="polite">Message sent! ✅</p>';
+    if (allFilled) {
+        popoverMessage.style.opacity = '0';
+    }
     
-    // Get the home link element and add focus to it
-    document.getElementById('homeLink').focus();
+    document.getElementById('submitButton').disabled = !allFilled;
+    return allFilled
 }
 
-// Here's your challenge:
-// 1. Update the code so that pressing the submit button 'politely' announces the new revealed submit message.
-// 2. Add focus to the home link when the user submits a message by selecting the correct element and adding ".focus()" to it.
+// Event listeners for each input field
+document.getElementById('name').addEventListener('input', checkInputs);
+document.getElementById('address').addEventListener('input', checkInputs);
+document.getElementById('city').addEventListener('change', checkInputs);
+document.getElementById('postcode').addEventListener('input', checkInputs);
+document.getElementById('email').addEventListener('input', checkInputs);
+document.getElementById('yes').addEventListener('change', checkInputs);
+document.getElementById('no').addEventListener('change', checkInputs);
+
+// click events are generally accessible to mouse, keyboard, and touchscreen users. Similarly, focus and blur can effectively replace mouseover and mouseout
+
+// For touchscreen users, events like touchstart can be used
