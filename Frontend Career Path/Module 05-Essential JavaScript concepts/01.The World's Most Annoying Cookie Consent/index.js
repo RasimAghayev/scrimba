07781114cd4af6bc9c22d1cd9510c1f4bf0cdec7
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-06 23:57:03 +0400

    Form Data 1

--- Frontend Career Path/Module 05-Essential JavaScript concepts/01.The World's Most Annoying Cookie Consent/index.js
+++ Frontend Career Path/Module 05-Essential JavaScript concepts/01.The World's Most Annoying Cookie Consent/index.js
@@ -13,6 +13,9 @@ modalCloseBtn.addEventListener("click", function () {
 
 consentForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  const consentFormData = new FormData(consentForm);
+  console.log(consentFormData);
+
   modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
