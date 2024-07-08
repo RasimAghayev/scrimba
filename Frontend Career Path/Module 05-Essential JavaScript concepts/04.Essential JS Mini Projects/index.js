fa52b2fb89d8c08d5f3f1fcc702f244d22e05156
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-07-08 13:52:04 +0400

    The .map() Method

--- Frontend Career Path/Module 05-Essential JavaScript concepts/04.Essential JS Mini Projects/index.js
+++ Frontend Career Path/Module 05-Essential JavaScript concepts/04.Essential JS Mini Projects/index.js
@@ -1,24 +1,15 @@
-const dreamHoliday = {
-    destination: 'Austin, Texas',
-    activity: 'visit the Tesla HQ',
-    accommodation: 'luxury ranch',
-    companion: 'Elon Musk'
+//Convert these Miles to KM! 
+const distanceWalkedMilesArr = [140, 153, 161, 153, 128, 148]
+
+const conversionFactorMilesToKm = 1.6
+
+function convertMilesToKms() {
+    return distanceWalkedMilesArr.map(function (distanceMiles, index) {
+        return `Month ${index}: ${distanceMiles * conversionFactorMilesToKm}KM`
+    })
 }
 
-const { destination, activity, accommodation, companion } = dreamHoliday
+console.log(convertMilesToKms())
 
-console.log(`I would love to go to ${destination} to ${activity}. 
-     I'd sleep in a ${accommodation} and hang out with ${companion} all day.`)
 
-/* 
-Challenge
-1. Complete the object dreamHoliday with whatever
-   information is true for you. Feel free to add 
-   extra properties or change the existing ones. 
-2. Destructure the object and use the individual 
-   variables to log out one or more sentences about 
-   your dream holiday. 
-   
-E.g. "I would love to go to Austin, Texas to visit the Tesla HQ. 
-     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
-*/
+
