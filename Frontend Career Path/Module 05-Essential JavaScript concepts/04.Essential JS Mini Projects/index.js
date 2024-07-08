e4399088a198a2ea1a248d5c5a9573711e68963b
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-07-08 13:56:25 +0400

    The .map() Method Challenge

--- Frontend Career Path/Module 05-Essential JavaScript concepts/04.Essential JS Mini Projects/index.js
+++ Frontend Career Path/Module 05-Essential JavaScript concepts/04.Essential JS Mini Projects/index.js
@@ -1,15 +1,27 @@
-//Convert these Miles to KM! 
-const distanceWalkedMilesArr = [140, 153, 161, 153, 128, 148]
+import { playlistArr } from '/playlist.js'
 
-const conversionFactorMilesToKm = 1.6
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
 
-function convertMilesToKms() {
-    return distanceWalkedMilesArr.map(function (distanceMiles, index) {
-        return `Month ${index}: ${distanceMiles * conversionFactorMilesToKm}KM`
-    })
-}
+const playlistHtml = playlistArr.map(function (track) {
+    return `
+<section class="card">
+    <div class="card-start">
+        <img src="/images/${track.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${track.title}</h4>
+            <p class="card-artist">${track.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+`
+})
 
-console.log(convertMilesToKms())
-
-
-
+document.getElementById('container').innerHTML = playlistHtml
